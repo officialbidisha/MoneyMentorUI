7cd2f7956885f8ac0a617035ac62a1f018d31167
Bidisha <officialbidisha1@gmail.com>
2020-09-07 19:19:54 +0530

    CRUD Completed

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,73 +1,151 @@
-import React, { Component } from 'react'
-import $ from '../../node_modules/jquery/dist/jquery'
+import React, { Component } from "react";
+import $ from "../../node_modules/jquery/dist/jquery";
+import axios from "axios";
 export class Modal extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            
-        }
-        this.modalref=React.createRef()
-    }
+    this.state = {
+      _id:null,
+      itemname: "",
+      price: "",
+      date: "",
+      categories: "",
+    };
 
-   showModal=()=>
-   (
-       $(this.modalref.current).modal({backdrop:"static"})
-   )
+    this.modalref = React.createRef();
+  }
 
-    render() {
-        return (
-            <div>
-                 <div ref={this.modalref} class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style={{display:"none"}} >
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button  type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <form>
-                                        <div class="form-group">
-                                            <label for="exampleFormControlInput1">Item Name</label>
-                                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Itemname" />
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleFormControlInput2">Price</label>
-                                            <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="Price" />
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleFormControlInput3">Date</label>
-                                            <input type="text" class="form-control" id="exampleFormControlInput3" placeholder="dd/mm/yyyy"  />
-                                           
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleFormControlSelect1">Categories</label>
-                                            <select class="form-control" id="exampleFormControlSelect1">
-                                                <option>Medicines</option>
-                                                <option>Groceries</option>
-                                                <option>Transportation</option>
-                                                <option>Housing & Rents</option>
-                                                <option>Toiletaries</option>
-                                                <option>Education</option>
-                                                <option>MakeUp</option>
-                                                <option>Clothing</option>
-                                            </select>
-                                        </div>
-                                    </form>
-                                    </div>
-      <div class="modal-footer">
-        <button  type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+  showModal = (item) =>{ 
+    if(item!=="") 
+    { this.setState({ _id:item._id, itemname:item.itemname, price: item.price, date: item.date, categories: item.categories } ) } $(this.modalref.current).modal({ backdrop: "static" });
+  };
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  saveHandler = (event) => {
+    console.log("save");
+    event.preventDefault();
+    console.log(this.state);
+    axios
+      .post(`http://localhost:8080/itemlist/create`, this.state, {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((res) => {
+        console.log(res.data);
+        this.props.mount();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { _id,itemname, price, date, categories } = this.state;
+    return (
+      <div>
+        <div
+          ref={this.modalref}
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+          style={{ display: "none" }}
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Add Item
+                </h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <form>
+                  <div class="form-group">
+                    <label for="exampleFormControlInput1">Item Name</label>
+                    <input
+                      type="text"
+                      value={itemname}
+                      name="itemname"
+                      class="form-control"
+                      placeholder="Itemname"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleFormControlInput2">Price</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      value={price}
+                      name="price"
+                      placeholder="Price"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleFormControlInput3">Date</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      name="date"
+                      value={date}
+                      placeholder="dd/mm/yyyy"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleFormControlSelect1">Categories</label>
+                    <select
+                      name="categories"
+                      value={categories}
+                      onChange={this.changeHandler}
+                      class="form-control"
+                    >
+                      <option>Medicines</option>
+                      <option>Groceries</option>
+                      <option>Transportation</option>
+                      <option>Housing & Rents</option>
+                      <option>Toiletaries</option>
+                      <option>Education</option>
+                      <option>MakeUp</option>
+                      <option>Clothing</option>
+                    </select>
+                  </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button
+                  type="submit"
+                  onClick={this.saveHandler}
+                  class="btn btn-primary"
+                  data-dismiss="modal"
+                >
+                  Save changes
+                </button>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Modal
+export default Modal;
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,63 +1,84 @@
-import ReactDOM, { render } from 'react-dom';
-import axios from 'axios';
-import React, { Component } from 'react'
-import {Link,Route} from 'react-router-dom'
-import Modal from './Modal';
+import ReactDOM, { render } from "react-dom";
+import axios from "axios";
+import React, { Component } from "react";
+import Modal from "./Modal";
 
 export class dashboard extends Component {
-    
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            arr:[],
-        }
-   this.modalRef=React.createRef()
-    }
-    
-   
+  constructor(props) {
+    super(props);
 
-    componentDidMount() {
-        
-        axios.get(`http://localhost:8080/itemlist/all`)
-          .then(res => {
-            const persons = res.data;
-            console.log(persons);
-            this.setState({arr:[...persons]});
-                    
-          })
-      }
+    this.state = {
+      arr: [],
+    };
+    this.modalRef = React.createRef();
+  }
+  componentDidMount = () => {
+    axios.get(`http://localhost:8080/itemlist/all`).then((res) => {
+      const persons = res.data;
+      console.log(persons);
+      this.setState({ arr: [...persons] });
+    });
+  };
+  deleteRow(id, e) {
+    axios.delete(`http://localhost:8080/itemlist/delete/${id}`).then((res) => {
+      console.log(res);
+      console.log(res.data);
+      const arr = this.state.arr.filter((item) => item._id !== id);
+      //arr.map(item=>console.log(item));
+      this.setState({ arr: arr });
+    });
+  }
 
-    
-        render() {
-            return (
-                <div class="container p-3">
-                    <div class="row">
-                            <div class="col-md-6 d-flex justify-content-center">Item</div>
-                            <div class="col-md-6 d-flex-justify-content-center">Price</div>
-                    </div>
-                    <hr></hr>
-                    {this.state.arr.map(item =>
-                         <div key={item._id}>
-                            <div class="row">
-                                <div class="col-md-6 d-flex justify-content-center">{item.itemname}</div>
-                                <div class="col-md-6 d-flex-justify-content-center">{item.price}</div>
-                            </div>
-                        </div>
+  render() {
+    return (
+      <div class="container p-3">
+        <div class="row">
+          <div class="col-6 d-flex justify-content-center">Item</div>
+          <div class="col-6 d-flex-justify-content-center">Price</div>
+        </div>
+        <hr></hr>
+        {this.state.arr.map((item) => (
+          <div key={item._id}>
+            <div class="row">
+              <div class="mb-1 col-6 d-flex justify-content-center">
+                {item.itemname}
+              </div>
 
+              <div class="mb-1 col-4 d-flex-justify-content-center">
+                {item.price}
+              </div>
 
-                    )}
-                    <div>
-               
-                        <button onClick={()=>this.modalRef.current.showModal()} type="button" className="btn btn-dark">
-                              Launch demo modal
-                        </button></div>
+              <div class="col-2 d-flex-justify-content-center">
+                <button
+                  class="btn btn-light"
+                  onClick={(e) => this.deleteRow(item._id, e)}
+                >
+                  Delete
+                </button>
+                <button
+                  class="btn btn-light"
+                  onClick={(e) => this.modalRef.current.showModal(item)}
+                >
+                  Update
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+        <div>
+          <div class="p-2"></div>
+          <button
+            onClick={() => this.modalRef.current.showModal("")}
+            type="button"
+            className="btn btn-dark"
+          >
+            Add
+          </button>
+        </div>
 
-                    <Modal ref={this.modalRef}/>
-                        
-                </div>
-
-            )
-        }
+        <Modal ref={this.modalRef} mount={this.componentDidMount} />
+      </div>
+    );
+  }
 }
-export default dashboard
+export default dashboard;
